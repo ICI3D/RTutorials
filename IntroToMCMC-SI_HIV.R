b17d8856eb2ba8e43579d5059b93ef9b126c7f84
Steve Bellan <steve.bellan@gmail.com>
2015-06-08 21:50:19 +0200

    made MCMC SI similar to MLE SI

--- IntroToMCMC-SI_HIV.R
+++ IntroToMCMC-SI_HIV.R
@@ -11,115 +11,120 @@
 ## * Calculate a binomial likelihood from these prevalence data and a fully specified epidemic model.
 ## * Be able to explain sequential, block, and adative Metropolis Hastings sampling algorithms.
 ## * Know how to assess multivariate MCMC chains for convergence both with trace plots and the Gelman-Rubin diagnostic.
+## * Create 95% credible intervals (CrI's) and contours from the posterior
 
-library(boot); library(deSolve); library(coda)
+require(boot); require(deSolve); require(ellipse); require(coda)
 
 ## Function that makes a list of disease parameters with default values
 disease_params <- function(Beta = 0.9
                            , alpha = 8 ## rate of beta decline with prevalence
-                           , progRt = 1-exp(-1/2.77) #(1/10)*4 ## rate of of progression through each of the I classes, for 10 years total
-                           , birthRt = .029 #.03 ## birth rate, 3% of people give birth per year
-                           , deathRt = .018 #1/60 ## 60 year natural life expectancy
+                           , progRt = (1/10)*4 ## rate of of progression through each of the I classes, for 10 years total
+                           , birthRt = .03 ## birth rate, 3% of people give birth per year
+                           , deathRt = 1/60 ## 60 year natural life expectancy
                            )
     return(as.list(environment()))
 
 disease_params()
 disease_params(Beta = .2)
 
-initPrev <- exp(-7) #10^-7 ## 1/10,000 infected at start
+initPrev <- exp(-7) ## infected at start
+tseqMonth <- seq(1976, 2015, by = 1/12)
 init <- c(S=1, I1=initPrev, I2=0, I3=0, I4=0, CI = 0, CD = 0) ## modeling proportion of population
-tseq <- seq(1976, 2015, by = 1/12)
 Is <- paste0('I',1:4) ## for easy indexing
 
-## SI ODE model
-SImod <- function(tt, yy, parms) with(parms, {
-    ## state variables
-    S <- yy[1]  ## Susceptibles
-    I1 <- yy[2] ## HIV stage 1
-    I2 <- yy[3] ## HIV stage 2
-    I3 <- yy[4] ## HIV stage 3
-    I4 <- yy[5] ## HIV stage 4
-    CI <- yy[6] ## cumulative incidence
-    CD <- yy[7] ## cumulative mortality
+## Define the SI ODE model. This model is equivalent to the model in HIV Spreadsheet #3. 
+SImod <- function(tt, yy, parms) with(c(parms,as.list(yy)), {
+    ## State variables are: S, I1, I2, I3, I4
     ## derived quantitties
     I <- I1+I2+I3+I4           ## total infecteds
     N <- I + S                 ## total population
     transmissionCoef <- Beta * exp(-alpha * I/N) ## Infectious contact rate
     ## state variable derivatives (ODE system)
-    deriv <- rep(NA,5)
-    deriv[1] <-	birthRt*N - deathRt*S - transmissionCoef*S*I/N
-    deriv[2] <-	transmissionCoef*S*I/N - progRt*I1 - deathRt*I1
-    deriv[3] <-	progRt*I1 - progRt*I2 - deathRt*I2
-    deriv[4] <-	progRt*I2 - progRt*I3 - deathRt*I3
-    deriv[5] <-	progRt*I3 - progRt*I4 - deathRt*I4
-    deriv[6] <-	transmissionCoef*S*I/N 
-    deriv[7] <-	progRt*I4
+    deriv <- rep(NA,7)
+    deriv[1] <-	birthRt*N - deathRt*S - transmissionCoef*S*I/N ## Instantaneous rate of change: Susceptibles
+    deriv[2] <-	transmissionCoef*S*I/N - progRt*I1 - deathRt*I1 ## Instantaneous rate of change: Infection class I1
+    deriv[3] <-	progRt*I1 - progRt*I2 - deathRt*I2 ## Instantaneous rate of change:  Infection class I2
+    deriv[4] <-	progRt*I2 - progRt*I3 - deathRt*I3 ## Instantaneous rate of change: Infection class I3 
+    deriv[5] <-	progRt*I3 - progRt*I4 - deathRt*I4 ## Instantaneous rate of change: Infection class I4
+    deriv[6] <-	transmissionCoef*S*I/N ## Instantaneous rate of change: Cumulative incidence
+    deriv[7] <-	progRt*I4 ## Instantaneous rate of change: Cumulative mortality
     return(list(deriv))
 })
 
-simEpidemic <- function(init, tseq, SImod=SImod, parms = disease_params()) {
-    simDat <- as.data.frame(lsoda(init, tseq, SImod, parms=parms))
+## Function to run the deterministic model simulation, based on the ODE system defined in SImod().
+simEpidemic <- function(init, tseq = tseqMonth, modFunction=SImod, parms = disease_params()) {
+    simDat <- as.data.frame(lsoda(init, tseq, modFunction, parms=parms))
     simDat$I <- rowSums(simDat[, Is])
     simDat$N <- rowSums(simDat[, c('S',Is)])
     simDat$P <- with(simDat, I/N)
-    simDat
+    return(simDat)
 }
 
+## Function to 'sample' the population:
 ## From a simulated epidemic, measure prevalence at several time points by performing
-## cross-sectional samples of individual at each time, testing them, and then calculated sample
+## cross-sectional samples of individuals at each time, testing them, and then calculating sample
 ## prevalence and associated binomial confidence intervals
-sampleEpidemic <- function(simDat, tseq = seq(1978, 2010, by = 2), numSamp = rep(100, length(tseq)), verbose=0) {
-    if(verbose>0) browser()
-    simDat$I <- rowSums(simDat[, Is])
-    prev_at_sample_times <- simDat[simDat$time %in% tseq, 'I']
+sampleEpidemic <- function(simDat # Simulated data (produced by a call to simEpidemic()) representing the 'true' underlying epidemic trajectory
+                           , sampleDates = seq(1980, 2010, by = 3) # Sample every 3 years from 1980 to 2010
+                           , numSamp = rep(80, length(sampleDates)) # Number of individuals sampled at each time point
+                           ) {
+    prev_at_sample_times <- simDat[simDat$time %in% sampleDates, 'P']
     numPos <- rbinom(length(numSamp), numSamp, prev_at_sample_times)
     lci <- mapply(function(x,n) binom.test(x,n)$conf.int[1], x = numPos, n = numSamp)
     uci <- mapply(function(x,n) binom.test(x,n)$conf.int[2], x = numPos, n = numSamp)    
-    return(data.frame(time = tseq, numPos, numSamp, sampPrev =  numPos/numSamp,
+    return(data.frame(time = sampleDates, numPos, numSamp, sampPrev =  numPos/numSamp,
                       lci = lci, uci = uci))
 }
 
 ## Run system of ODEs for "true" parameter values
-set.seed(4)
-trueParms <- disease_params()#Beta = .9, alpha = 8, progRt = 1/2.5)
-simDat <- simEpidemic(init, tseq, SImod, parms = trueParms)
+trueParms <- disease_params() # Default model parameters are defined in lines 20-26
+simDat <- simEpidemic(init, parms = trueParms) # Simulated epidemic (underlying process)
+
 par(bty='n', lwd = 2)
-with(simDat, plot(time, P, xlab = '', ylab = 'prevalence', type = 'l', ylim = c(0,.4), col='red'))
+# Plot simulated prevalence through time:
+with(simDat, plot(time, P, xlab = '', ylab = 'prevalence', type = 'l', ylim = c(0,.4), col='red', las = 1))
+## Take cross-sectional sample of individuals to get prevalence estimates at multiple time points:
+set.seed(1) # Initiate the random number generator (so everyone's simulation looks the same)
+myDat <- sampleEpidemic(simDat) # Simulate data from the sampling process (function defined above)
+points(myDat$time, myDat$sampPrev, col = 'red', pch = 16, cex = 2) # Plot sample prevalence at each time point
+arrows(myDat$time, myDat$uci, myDat$time, myDat$lci, col = 'red', len = .025, angle = 90, code = 3) # Plot 95% CI's around the sample prevalences
 
-## Take cross-sectional sample of individuals to get prevalence estimates at multiple time points
-myDat <- sampleEpidemic(simDat, verbose = 0)
-points(obsDat$time, obsDat$sampPrev, col = 'red', pch = 16, cex = 2)
-arrows(obsDat$time, obsDat$uci, obsDat$time, obsDat$lci, col = 'red', len = .025, angle = 90, code = 3)
+## To start, we need to write a -log likelihood function that gives the probability that a given parameter set
+## (Beta, alpha values) would generate the observed data. Remember that we are assuming that there is some
+## true underlying epidemic curve that is deterministic and the data we observe are only noisy
+## because of sampling/observation error (not because the underying curve is also
+## noisy--i.e. process error--which would be particularly likely for epidemics in small populations).
 
-## Log-Likelihood
-llikelihood <- function(parms = disease_params(), obsDat=myDat, verbose = 0) {
-    simDat <- as.data.frame(lsoda(init, tseq, SImod, parms=parms))
-    simDat$I <- rowSums(simDat[, Is])
-    simDat$N <- rowSums(simDat[, c('S',Is)])
-    simDat$P <- with(simDat, I/N)
-    if(verbose > 5) browser()
+## We assume binomial sampling errors. So we can write the -log-likelihood as the probability of
+## observing the observed number positive out of each sample if the prevalence is the value generated by
+## a model parameterized by a given set of parameters:
+
+llikelihood <- function(parms = disease_params(), obsDat=myDat) {
+    simDat <- simEpidemic(init, parms=parms)
+    ## What are the rows from our simulation at which we have observed data?
     matchedTimes <- simDat$time %in% obsDat$time
     lls <- dbinom(obsDat$numPos, obsDat$numSamp, prob = simDat$P[matchedTimes], log = T)
     return(sum(lls))
 }
-llikelihood()
+llikelihood(trueParms) ## loglikelihood of the true parameters (which we usually never know)
+llikelihood(disease_params(Beta = 3, alpha = 1))  ## vs some random guessed parameters
 
 ## Log-Prior (assume uninformative)
 lprior <- function(parms=disease_params()) with(parms, {
-    lp <- 0
+    lp <- 0     ## whatever the parameters are, assume they have the same probability
     return(lp)
 })
 
 llikePrior <- function(fit.params=NULL, ## parameters to fit
                        ref.params = disease_params(), ## reference parameters
-                       obsDat, verbose = 0) { ## observed data
+                       obsDat=myDat) { ## observed data
     parms <- within(ref.params, { ## subs fitting parameters into reference parameter vector
         for(nm in names(fit.params)) assign(nm, as.numeric(fit.params[nm]))
         rm(nm)
     })
-    llikelihood(parms, obsDat=obsDat, verbose = verbose) + lprior(parms)
+    llikelihood(parms, obsDat=obsDat) + lprior(parms)
 }
-llikePrior(obsDat=obsDat)
+llikePrior(obsDat=myDat)
 
 logParms <- function(fit.params) {
     fit.params <- log(fit.params)
@@ -133,6 +138,7 @@ unlogParms <- function(fit.params) {
 }
 unlogParms(logParms(c(alpha = 3, Beta=.3)))
 
+## set bounds on initial parameter guesses
 initBounds <- data.frame(rbind( ## for initial conditions
                                c(log(.2),log(2)) ## beta
                                ,c(log(1), log(30)) ## alpha
@@ -142,6 +148,7 @@ rownames(initBounds) <- c('logBeta','logalpha','logprogRt')
 class(initBounds[,2]) <- class(initBounds[,1]) <- 'numeric'
 initBounds
 
+##  randomly select a value that is uniformly distributed between these bounds
 initRand <- function(fit.params) {
     fit.params <- logParms(fit.params)
     tempnm <- names(fit.params)
@@ -149,10 +156,12 @@ initRand <- function(fit.params) {
                                               max =  initBounds[row.names(initBounds)==nm, 'upper'])
     return(unlogParms(fit.params))
 }
-initRand(c(alpha = 3, Beta = 1))
+## give it parameter vector and it will simulate random values within the bounds for those values
+initRand(c(alpha = 3, Beta = 1)) 
+initRand(c(alpha = NA, Beta = NA)) ## independent of the inputs, just requires the parameter vector names
 
-
-mcmcSampler <- function(current.params, ref.params=disease_params(), obsDat, seed = 1,
+mcmcSampler <- function(init.params, ref.params=disease_params(),
+                        obsDat = myDat, seed = 1,
                         proposer = sequential.proposer(sdProps=sdProps),
                         adaptiveMCMC = F, startAdapt = 300,
                         plotterLoops = NULL, ## repeat plot of some iterations more to lengthen their display
@@ -160,11 +169,12 @@ mcmcSampler <- function(current.params, ref.params=disease_params(), obsDat, see
                         plotter = plotterTS, randInit = T, niter = 100, nburn = 0, adptBurn = 200,
                         verbose=0, plotNM=NULL, tell = 100) {
     if(verbose>2) browser()
-    if(randInit) current.params <- initRand(current.params)
+    if(randInit) init.params <- initRand(init.params)
+    current.params <- init.params
     nfitted <- length(current.params)
     vv <- 2 ## mcmc iteration
     accept <- 0
-    curVal <- llikePrior(current.params, ref.params = ref.params, obsDat=obsDat, verbose = verbose)
+    curVal <- llikePrior(current.params, ref.params = ref.params, obsDat=obsDat)
     out <- matrix(NA, nr = niter, nc=length(current.params)+1)
     out[1,] <- c(current.params, nll = -curVal)
     colnames(out) <- c(names(current.params), 'nll')
@@ -178,9 +188,8 @@ mcmcSampler <- function(current.params, ref.params=disease_params(), obsDat, see
         ## adapt covariance every 50 iterations
         if(adaptiveMCMC & proposer$type=='block' & vv > startAdapt & vv %% 50 == 0) {
             adptBurn <- min((startAdapt-50), adptBurn)
-            save(list = ls(all.names = TRUE), file='dbgAdapt.Rdata')
-            ## load(file='dbgAdapt.Rdata')
-            adaptedCovar <- 2.38^2 / nfitted * cov.wt(log(out[adptBurn:(vv-1),1:nfitted]))$cov ## will converge for vv large
+            ## will converge for vv large
+            adaptedCovar <- 2.38^2 / nfitted * cov.wt(log(out[adptBurn:(vv-1),1:nfitted]))$cov 
             adaptedCovar <- adaptedCovar*.95 + originalCovar*.05 ## 95% adapted & 5% original
             rownames(adaptedCovar) <- colnames(adaptedCovar) <- names(current.params)
             assign('covar', adaptedCovar, envir = environment(proposer$fxn))
@@ -189,7 +198,7 @@ mcmcSampler <- function(current.params, ref.params=disease_params(), obsDat, see
         onpar <- proposal0$onpar
         propt <- proposal0$type
         proposal <- unlogParms(proposal0$proposal)
-        propVal <- llikePrior(proposal, ref.params = ref.params, obsDat=obsDat, verbose = verbose)
+        propVal <- llikePrior(proposal, ref.params = ref.params, obsDat=obsDat)
         lmh <- propVal - curVal ## likelihood ratio = log likelihood difference
         if (is.na(lmh)) { ## if NA, print informative info but don't accept it
             print(list(lmh=lmh, proposal=exp(proposal), vv=vv, seed=seed))
@@ -205,14 +214,60 @@ mcmcSampler <- function(current.params, ref.params=disease_params(), obsDat, see
         out[vv, ] <- c(current.params, nll=curVal)
         vv <- vv+1
         aratio <- accept/((vv-nburn))
-        if(!is.null(plotter)) {
-                par(opar)
-                plotter(out, vv, ref.params=ref.params, obsDat=obsDat, proposer = proposer, proposal = proposal,
-                        onpar=onpar,proptype=propt, aratio = aratio, showing = showing, plotterLoops=plotterLoops)
-        }
     }
-    if(!is.null(plotNM)) graphics.off()
     colnames(out) <- c(names(current.params), 'nll')
-    return(list(out = out[1:nrow(out)>(nburn+1),], aratio = aratio, current.params = current.params,
-                ref.params=ref.params))
+    samp <- as.mcmc(out[1:nrow(out)>(nburn+1),])
+    return(list(ref.params=ref.params
+              , seed = seed
+              , init.params = init.params
+              , aratio = aratio
+              , samp = samp
+                ))
 }
+
+## Sequential proposal function: Propose one parameter at a time
+sequential.proposer <- function(sdProps) {
+    nfitted <- length(sdProps)
+    on <- 0
+    return(list(sdProps = sdProps, type = 'sequential',
+                fxn = function(current) {
+                    proposal <- current
+                    proposal[on + 1] <- proposal[on + 1] + rnorm(1, mean = 0, sd = sdProps[on + 1])
+                    on <<- (on+1) %% nfitted
+                    list(proposal=proposal, onpar=on+1, type = 'sequential')
+                }))
+}
+
+## Propose parameters within blocks
+multiv.proposer <- function(covar, blockLS = list(rownames(covar))) {
+    nblocks <- length(blockLS)
+    on <- 0
+    return(list(type = 'block',
+                fxn = function(current) {
+                    proposal <- current + rmnorm(1, mean = 0, varcov = covar)
+                    propsosal <- as.vector(proposal)
+                    names(proposal) <- names(current)
+                    list(proposal=proposal,  type = 'block', covar=covar, onpar = NA)
+                }))
+       }
+
+samp_Seq <- mcmcSampler(c(alpha=8, Beta=.9)
+                      , seed = 1
+                      , proposer = sequential.proposer(sdProps=c(.15,.15))
+                      , randInit = T
+                      , niter = 100
+                      , nburn = 0
+                      , verbose=0)
+
+class(samp_Seq$samp)
+## The coda package already knows how to plot MCMC objects by default
+par('ps'=16, las = 1)
+plot(samp_Seq$samp)
+
+## Try changing the above to have niter = 1000. How does this affect the trace plots? Use
+## Gelman-Rubin diagnostic to test convergence.
+## Q3
+chainList <- as.mcmc.list(list(posteriorSample1, posteriorSample2, posteriorSample3, posteriorSample4))
+class(chainList)
+gelman.diag(chainList)
+
