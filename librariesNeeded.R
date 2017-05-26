4870ea53f668c8b38e1264eb3c5bf24b1780c75d
Steven Bellan <stevenbellan@aims-usbc-ethernet-adaptor.aims.ac.za>
2017-05-26 16:11:56 +0200

    R libraries needed

---
+++ librariesNeeded.R
@@ -0,0 +1,20 @@
+libsToLoad <- c(
+    ,"deSolve"
+    ,"epiDisplay"
+    ,"boot"
+    ,"coda"
+    ,"stats4"
+    ,"date"
+    ,"shiny"
+    ,"dplyr"
+    ,"tidyverse"
+    ,"ellipse"
+    ,"parallel"
+    ,"mnormt"
+    ,"emdbook"
+    ,"Matrix"
+   ,"R0"
+#    ,"OutbreakTools"
+)
+install.packages(libsToLoad)
+
