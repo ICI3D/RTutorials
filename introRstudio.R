de7c5466a55b3e1e65601f323159f82584fa5b21
jrcpulliam <juliet@ici3d.org>
2019-05-11 17:03:54 +0200

    update Rstudio intro tutorial

--- introRstudio.R
+++ introRstudio.R
@@ -5,13 +5,15 @@
 ##
 ## Dario Fanucchi, May 2012
 ## Updated by Juliet R.C. Pulliam, December 2014
+## Updated by Juliet R.C. Pulliam, May 2019
 ## Some Rights Reserved
 ## CC BY-NC 3.0 (http://creativecommons.org/licenses/by-nc/3.0/)
 ##
 ##
-## RStudio is an Integrated Development Environment (IDE) for the R programming language. Much like Emacs,
-## Vim, Gedit and other editors; RStudio provides facilities for editing source files, running lines or
-## sections of code in the ## R console, typing into the console directly, and exploring/changing the 
+## RStudio is an Integrated Development Environment (IDE) for the R programming 
+## language. Much like Emacs, Vim, Gedit and other editors; RStudio provides 
+## facilities for editing source files, running lines or sections of code in the 
+## R console, typing into the console directly, and exploring/changing the 
 ## working directory. RStudio also offers some R-specific behavior, such as
 ## keeping track of the variables currently in the workspace and showing the
 ## command history.
@@ -20,7 +22,7 @@
 ## maximize your productivity when working through R tutorials. Once you've
 ## worked through the tutorial, you should be able to:
 ##
-##  - understand what the various windows in RStudio are for
+##  - understand the various windows in RStudio 
 ##  - change the working directory and open files in RStudio
 ##  - run code from the console window or the source window
 ##  - understand what the workspace is and how to change the value(s) of
@@ -30,21 +32,21 @@
 ##  - use the manipulate package to make interactive plots
 ##  - make use of the Help system
 ##  - use shortcut keys
-
-
+##
 ## If you are not reading this file from within RStudio, you can open it in
 ## RStudio in one of several ways:
 ##
 ##  - Open the file using "File"->"Open File" in the RStudio editor
 ##  - At the terminal/cmd write the following:
-##  rstudio Rstudio_intro.r
-##  - In RStudio in the files window (bottom right of the screen or ctrl+5 to
+##  rstudio introRstudio.R (or, on a Mac: open -a RStudio introRstudio.R)
+##  - In RStudio in the files window (bottom right of the screen or Ctrl+5 to
 ##  activate), browse to the location of the file by clicking the "..." to the
 ##  top-right of the window, and then open the directory. Click on the file in
 ##  the files window and it will load in the editor.
-
+##
 ## Note that - throughout the tutorial - Mac users should use the command (open
-## apple) key if the keystroke sequence calls for the control key.
+## apple) key if the keystroke sequence calls for the control key but does not
+## appear to function as expected.
 
 ################################################################################
 # 1. Using RStudio to run line-by-line from scripts
@@ -72,7 +74,7 @@ plot(c(1,2,3,4,5), c(1,2,3,4,5), "l")
 # "Control+Enter" to execute commands in the console.
 
 ################################################################################
-# 2. Variables, the Workspace, and basic arithmetic
+# 2. Variables, the Environment, and basic arithmetic
 ################################################################################
 
 # In R you can assign a value to a variable by using the "<-" operator.
@@ -80,8 +82,8 @@ plot(c(1,2,3,4,5), c(1,2,3,4,5), "l")
 
 x <- 10
 
-# Make sure the Workspace window is visible by typing "Control+3".
-# Notice that the Workspace window now contains a variable x, with a value of
+# Make sure the Environment window is visible by typing "Control+3".
+# Notice that the Environment window now contains a variable x, with a value of
 # 10. You can edit x in the workspace window by clicking on x and typing a new
 # value. Try changing the value of x to 15. Then run the following line (with
 # control+Enter) to print it in the console.
@@ -92,7 +94,7 @@ x
 
 remove(x)
 
-# Notice how x no longer shows up in the Workspace window. Typing x now produces
+# Notice how x no longer shows up in the Environment window. Typing x now produces
 # an error:
 
 x  # will produce an error: object 'x' not found!
@@ -100,7 +102,7 @@ x  # will produce an error: object 'x' not found!
 # R can be used to perform arithmetic in a similar fashion to a calculator.
 # For instance, you can type this:
 
-(1242-241.1)*32.21
+(1242-241.1) * 32.21
 
 # When you press control+Enter, the cursor automatically moves to the next line
 # in the console. This way, you can run scripts one line at a time by repeatedly
@@ -108,11 +110,11 @@ x  # will produce an error: object 'x' not found!
 
 # The following code does some basic arithmetic.  Run it line by line:
 
-x <- 2             # ask someone to guess a number between 1 and 10
-y <- x*9           # tell them to multiply it by 9
-d1 <- floor(y/10)  # get the first digit  (floor(x) gives the integer part of x)
-d2 <- y%%10        # get the second digit (a%%b gives the remainder when a is divided by b)
-d1+d2-4            # tell them to add the digits and subtract 4: they are left with the number 5!
+x <- 2              # ask someone to guess a number between 1 and 10
+y <- x * 9          # tell them to multiply x by 9
+d1 <- floor(y / 10) # get the first digit  (floor(x) gives the integer part of x)
+d2 <- y %% 10       # get the second digit (a %% b gives the remainder when a is divided by b)
+d1 + d2 - 4         # tell them to add the digits and subtract 4: they are left with the number 5!
 
 # The code above performed an old classic arithmetic "teaser". Starting with any
 # number (x). Multiply it by 9, add the digits of the result, subtract 4 and
@@ -127,10 +129,11 @@ d1+d2-4            # tell them to add the digits and subtract 4: they are left w
 # added to the workspace with their values
 # You can see in the workspace that d1 and d2 are indeed the first and second
 # digit of y respectively.
+
 # Since we're done with these variables, we'll keep our workspace clean by
 # removing them:
 
-remove(x,y,d1,d2)
+rm(x,y,d1,d2)
 
 
 ################################################################################
@@ -149,66 +152,56 @@ remove(x,y,d1,d2)
 
 # Sometimes you don't want to run your code line-by-line but rather in a batch.
 # In RStudio you can simply highlight the code you want to run and press
-# "Control+Enter"
-# just as before to run all the code you highlighted.
+# "Control+Enter" just as before to run all the code you highlighted.
 
 # The code below computes the prime numbers in the range 1-100 and writes them
-# in the console.
-# There are many control structures and commands in this code, and it might
-# feel excessive to run it line-by line. Don't worry about how it works for the
-# moment
-# (although there are line-by-line comments for the very enthusiastic reader)
-# In order to run it all in one shot, highlight the whole section and press
-# "Control+Enter"
+# in the console. There are many control structures and commands in this code, and 
+# it might feel excessive to run it line-by line. Don't worry about how it works 
+# for the moment (although there are line-by-line comments for the very
+# enthusiastic reader). In order to run it all in one shot, highlight the whole 
+# section and press "Control+Enter".
 
-x <- lapply(1:100, function(x)TRUE)              # make a vector of 100 elements all equal to TRUE
-prim <- NULL                                     # Empty list of currently known primes
-x[1] <- FALSE                                    # 1 is not a prime number
-for(i in 2:100){                                 # go through the list from 2 to 100
-  if(x[i]==TRUE) {                               # if i is known to be prime...
-    prim <- append(prim, i)                      #    ... add it to the list of known primes
-    if(i<=50)                                    #    ... if i has multiples in [1,100] ...
-      for(j in i*(2:(100/i))  ) x[j]=FALSE       #         ... and set all multiples of i to be NOT prime
+x <- rep(TRUE,100)              # make a vector of 100 elements all equal to TRUE
+prim <- NULL                    # Empty list of currently known primes
+x[1] <- FALSE                   # 1 is not a prime number
+for(i in 2:100){                # go through the list from 2 to 100
+  if(x[i]==TRUE) {                  # if i is known to be prime...
+    prim <- append(prim, i)             # ... add it to the list of known primes
+    if(i<=50)                           # ... if i has multiples in [1,100] ...
+      for(j in i*(2:(100/i))  ) x[j]=FALSE    # ... and set all multiples of i to be NOT prime
   }
 }
-print(prim)                                      # print the list of discovered primes to the console
+print(prim)                     # print the list of discovered primes to the console
 
 # Notice that all the code gets "typed" into the console when you press
 # Control+Enter. The list of primes below 100 is produced.
 
-
 ################################################################################
-# 3. Vectors/Lists in R
+# 3. Vectors in R
 ################################################################################
 
 # Look at your workspace (Ctrl+3 if it isn't showing). There are two variables i
 # and j that are single valued variables similar to those we've seen before. i
-# has a value of 100L. The L just refers to
-# the data type associated with i: in this case it is a "long integer". You can
-# edit its value by clicking on it, just as we did with x earlier. You can also
-# edit the value of j.
+# has a value of 100L. The L just refers to the data type associated with i: in 
+# this case it is a "long integer". 
 # There are also two other variables in your workspace that are not as familiar:
 # x and prim. These are vectors.
 # Next to prim is a summary of the structure of the data it contains:
-# integer[25]. This says that prim is a vector containing 25 integers.
+# int [1:25]. This says that prim is a vector containing 25 integers.
 
-# Click on prim in the workspace.
-# A window opens up with the contents of the list. It is preceeded by the letter
-# "c". This is how vectors are represented in R.
-# Indeed we can make a new list by using the "c" (concatenate) command:
+# We can make a new vector by using the "c" (concatenate) command:
 
-mylist <- c(1.1, 2.2, 3.3, 4.4, 5.5, 4.4, 3.3, 2.2, 1.1)
+myvect <- c(1.1, 2.2, 3.3, 4.4, 5.5, 4.4, 3.3, 2.2, 1.1)
 
-# You can click on mylist in the workspace to see (and edit) its contents.
-# Notice that it is summarized as numeric[9]. It contains 9 elements, each of
+# Notice that it is summarized as num [1:9]. It contains 9 elements, each of
 # which is a numeric data type.
 
 # We now have quite a few variables hanging around in the workspace.
 
-# Clean up the entire workspace by clicking the "Clear All" button or typing the
-# following code:
+# Clean up the entire workspace by clicking the "Clear All" button, which looks 
+# like a broom, or typing the following code:
 
-remove(list=ls())
+rm(list=ls())
 
 ################################################################################
 # 5. Some RStudio-specific tips and tricks
@@ -239,44 +232,38 @@ med
 # option: Press F1 while hovering over a valid R function or command. For
 # example, try the completion of the last statement above.
 
-# Press F1 over the line of code below:
+# Press F1 over the line of code below (fn+F1 on a Mac):
 
 median
 
 # Notice that the help window (my bottom right corner) loads the help for
-# "Median Value".
-# This feature is very useful indeed, and it can increase your productivity in R
-# substantially
-# to be able to obtain help very quickly on functions you wish to use in this
-# fashion.
+# "Median Value". This feature is very useful indeed, and it can increase your 
+# productivity in R substantially to be able to obtain help very quickly on 
+# functions you wish to use in this fashion.
 
 ##### Console History and History tab
 
 # As previously mentioned it is possible to obtain previous statements typed
-# into the
-# console by pressing the up arrow when inside the console.
+# into the console by pressing the up arrow when inside the console.
 # If you would like a bit more control over your previous statements, you can
-# open the
-# History window.
+# open the History window.
 # The History window can be activated by typing Ctrl+4, or by clicking on the
-# History tab
-# to the left. Inside the History window, you can scroll through all code
-# previously run
-# in the console.
+# History tab. Inside the History window, you can scroll through all code
+# previously run in the console.
 # Double clicking an entry in the History window (or clicking Enter on it)
 # will cause it to copy to the console, where it can be evaluated.
 # Alternatively, you can click the "To Source" button (or type Shift+Enter)
 # to send the highlighted line(s) to the current location in the source editor.
 
-#As an example, run the following lines of code:
+# As an example, run the following lines of code:
 
 x <- 1
 y <- 1
 
 # and now run the following block of code
 
-x <- x*(x%%2+1)+1
-y <- y*(x+1)
+x <- x * (x %% 2 + 1) + 1
+y <- y * (x + 1)
 
 # Place the cursor in the source below this comment, then look in the history
 # window for the above two lines, highlight them and click Shift+Enter. Observe
@@ -290,7 +277,7 @@ y <- y*(x+1)
 
 ##### Including Packages
 
-# To load an already-installed package, you can go to the packages tab (ctrl+7),
+# To load an already-installed package, you can go to the packages tab (Ctrl+7),
 # and select the package that you want to use. For instance, you can include the
 # Matrix package in this fashion. You can also do this in code as follows:
 
@@ -302,8 +289,8 @@ require("Matrix")
 # package
 
 M <- Matrix(0, 2, 2, sparse=FALSE)
-M[1,1] = 2; M[1,2] = 3
-M[2,1] = 3; M[2,2] = 4;
+M[1,1] <- 2; M[1,2] <- 3
+M[2,1] <- 3; M[2,2] <- 4
 M
 det(M)
 
@@ -312,13 +299,14 @@ det(M)
 # 6. Running the whole script
 ################################################################################
 
-# There are a few more options for running code in RStudio other than writing
+# There are a few more options for running code in RStudio other than typing
 # "Control+Enter". They are provided here with their shortcuts for completeness.
-#     1. Run the ENTIRE script:                              "Control+Shift+R"
+#     1. Run the ENTIRE script:                              "Control+Shift+O"
 #     2. Run the script FROM THE BEGINNING UP TO THIS LINE:  "Control+Shift+B"
-#     3. Run the script FROM THIS LINE TO THE END:           "Control+Shift+E"
+#     3. Run the script FROM THIS LINE TO THE END:           "Control+Shift+EÓ"
 # These can all be useful in different contexts. You can try them out by moving
 # to various places in this script and typing these key combinations.
+# For additional keyboard shortcuts, see Tools > Keyboard Shortcuts Help.
 
 ################################################################################
 # Appendix. List of useful RStudio Shortcut keys
@@ -328,7 +316,7 @@ det(M)
 
 # Ctrl+1          (Move cursor to Editor)
 # Ctrl+2          (Move cursor to Console)
-# Ctrl+3          (Show workspace)
+# Ctrl+3          (Show Environment window)
 # Ctrl+Enter      (Editor: run a line, or a highlighted section of code)
 # Ctrl+L          (clear the console)
 # Ctrl+Up arrow   (Console: see list of previous commands; Editor: Go to top of
