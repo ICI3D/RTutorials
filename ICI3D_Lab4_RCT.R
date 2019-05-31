cd096b0c57aab34432b1fc1361897fcbc9aa9788
Jonathan Dushoff <dushoff@mcmaster.ca>
2019-05-31 03:42:33 -0400

    Formatting; a little bit of explanatory and interrogatory clarification.

--- ICI3D_Lab4_RCT.R
+++ ICI3D_Lab4_RCT.R
@@ -36,7 +36,7 @@
 #In some of these, we are interested in a binary (yes/no) outcome, such as the occurrence of stomach cancer.  In others,
 #we are interested in an event rate (such as the frequency of migraine), a continuous variable (hearing loss), or a 
 #possibly censored event time (time to HIV infection).  
-#
+
 #Friedman et al. [Friedman1998] define a clinical trial as a prospective study comparing the effect of
 #an intervention against a control in humans.  The key features are
 # A clinical trial is prospective, so that the assignment is made and then the patients are followed forward in time.
@@ -44,7 +44,7 @@
 # A clinical trial is an experiment.
 # Clinical trials are governed by ethical principles, and therefore differ from experimental studies in
 #other fields where patient safety is not a factor.
-#
+
 #While some of these questions were suitable for a clinical trial, some questions of interest cannot be studied
 #experimentally.  In some cases, the exposure of interest is
 #known to cause other health problems, and cannot be experimentally administered (cigarette smoking), or the outcome
@@ -52,23 +52,23 @@
 #simply be impractical because of adherence, while in others, the rationale for a clinical trial is 
 #dubious on subject matter grounds (e.g., it is unclear on a priori grounds whether or not prayer is
 #an appropriate subject for a clinical trial).
-#
+
 #What can clinical trials do that other designs cannot?
 #Clinical trials permit causal inference.  By experimentally manipulating the exposure, confounding is (in a 
 #probabilistic sense) eliminated.  Moreover, good clinical trial protocols minimize bias by using masked
 #patients and investigators.  Masking and randomization are the twin cornerstones of clinical trial design.
-#
+
 #Why is masking important?
 #Masking, also called  blinding, removes an important source of bias.  For example, consider a trial of
 #whether or not acupuncture prevents migraine.  Patients or investigators may have a prior commitment to acupuncture,
 #which is a low-tech traditional intervention.  Belief in the efficacy of the intervention may lead people to 
 #inadvertently understate the frequency of migraine if the patients know they received acupuncture.  This bias may
 #lead people to believe in the efficacy of the intervention.
-#
+
 #When both the
 #subjects and investigators are masked, the trial is {\it double masked} (also called
 # double blind).  
-#
+
 #In certain trials, masking is impossible.  For example, in a trial of a surgical procedure, it
 #is impossible to mask the surgeon, and frequently impossible to mask the patients (it would be impossible
 #for a patient randomized to radical prostatectomy vs external beam radiation to be masked).  Another
@@ -76,17 +76,17 @@
 #been randomized to a low carbohydrate vs a low fat arm (for example).
 #In such circumstances, it may still be possible to use biomedical measurements made by masked personnel, so that
 #the inference could be based on objective measurements made by masked personnel.
-#
+
 #Unmasked trials are viewed as less credible than masked trials, because of bias.  Therefore, both subjects 
 #and investigators should be masked whenever possible.  When evaluating a published clinical trial, be aware of
 #subtle ways in which masking could have been compromised.  For instance, patients may be aware of side effects of
 #the medication, or the medications may be different in some subtle way detectable to trained physicians.
-#
+
 #Why is randomization important?
 #Randomization insures that unknown factors will be balanced in the study arms, in a probabilistic sense.  The 
 #development of randomized experimental designs by R. A. Fisher early in the last century arguably inaugurated the modern
 #era of biomedical research.
-#
+
 #Armitage [Armitage1982] gives three important reasons for randomization.  First, randomization achieves broad similarity
 #between the treatment and control groups, with respect to known and unknown prognostic factors; it is unlikely that a
 #serious imbalance will occur by chance alone.  
@@ -97,43 +97,43 @@
 #clinicians would be certain that the next patient to be admitted would, say, receive the treatment, and their decision
 #whether to accept this patient might consequently be affected.''  Yoshioka goes on to give a fourth reason in support of
 #randomization in a clinical trial: fairness in allocation.
-#
+
 #%In Exercise 1, we will simulate a clinical trial.  Suppose that recovery from tuberculosis is governed by an unknown
 #%individual factor, something we cannot measure.  Assume that the probability of recovery is 18/107, taken from 
 #%both arms of the MRC tuberculosis trial of 1948.  Simulate a clinical trial in which 55 patients (110 in total) are assigned 
 #%to a treatment or a control group, supposing that the intervention does not work.  
-#
+
 #Why is a control group important?
 #Without a control group, we do not know what would have happened without the treatment, and therefore cannot estimate
 #the effect of the treatment.  The control group may be a masked placebo, or in some cases may be an existing standard of
 #care to which a new intervention is being compared.
-#
+
 #When is a clinical trial needed?
 #Although a clinical trial provides the highest form of evidence, the expense of a clinical 
 #trial implies that a clinical trial should only done when the question is of sufficient importance
 #to warrant a trial.  
-#
+
 #Equipoise is indicated prior to beginning a clinical trial.  There must be genuine disagreement as to
-#the value of the therapy.  For instance, topical application of the antifungal {\it natamycin} is commonly
+#the value of the therapy.  For instance, topical application of the antifungal _natamycin_ is commonly
 #used for fungal infections of the cornea, although no controlled clinical trial ever established this.  The reason
-#is that it seems better on {\it a priori} grounds to use an antifungal agent to treat a fungal infection
+#is that it seems better on _a priori_ grounds to use an antifungal agent to treat a fungal infection
 #than to offer no treatment to the patient.
-#
+
 #What are some common clinical trial designs?
 #What is a factorial design?
 #A factorial design assigns study units to all combinations of experimental factors.  In the Mycotic Ulcer 
 #Therapeutic Exploratory Trial, the investigators were interested in drugs and in corneal scraping.  Two drugs were
 #of interest (natamycin and voriconazole, making two levels of this factor), and two scraping protocols (either
 #actually scrape, or don't scrape, making two levels of this factor).  A $2 \times 2$ factorial design was conducted.
-#
+
 #What is a noninferiority trial?
 #In some cases, we are interested in a new drug for which there is already an accepted drug or treatment.  It is
 #unethical to withhold treatment, so that ethical considerations preclude the presence of a placebo control.  In this case, we
 #wish to test the hypothesis that the new drug is not worse than the old drug.
-#
+
 #What is a group-randomized trial?
 #In some cases, the natural intervention unit is not the individual, but the group or community.
-#
+
 #How can one analyze data from a simple trial?
 #Statistical analysis of clinical trial data depends on the nature of the outcome variable and the
 #trial design.  In the examples below, we will illustrate analysis using data from the 
@@ -148,31 +148,26 @@
 #To simulate a given number of random variates, use the {\tt rnorm} command.  Here, we simulate 100 
 #standard normal variates with a mean of 10 and a standard deviation of 3:
 
-
 v1 <- rnorm(100,mean=10,sd=3)
 hist(v1)
 
-
-
 #Simulate 30 variates with a mean of 1 and a standard deviation of 0.3.  Repeat this exercise 10 times, 
 #plotting the histogram each time.
+# Describe what you find
 
 #Random selection
 #How can we simulate random selection of people into a treatment and a control group?  To do so,
-#we use sampling without replacement based on the sample command.  Here, we divide 20 subjects
+#we use sampling without replacement based on the `sample` command.  Here, we divide 20 subjects
 #into a treatment group and a control group.  We select without replacement half of the numbers from
 #1 to 20 (representing the people) into a treatment and a control group.
-
 
 indices <- 1:20
 treatment.group <- sample(indices,size=10,replace=FALSE)
 treatment.group
 
-
 #Repeat this exercise a few times.
 
 #Another way to do it is to simply shuffle a vector of 0s and 1s.  
-
 
 assignments <- sample( c(rep(1,10),rep(0,10)), size=20, replace=FALSE )
 assignments
@@ -186,7 +181,7 @@ assignments
 which(assignments==1)
 
 
-#
+
 #Working with data frames
 #We will now generate a simulated data set.   Here, we assume the treatment and control groups 
 #do not differ statistically---the mean and standard deviation of the outcome variable are assumed
@@ -200,7 +195,7 @@ dset$outcome <- rnorm(n.subjects, mean=1,sd=0.3)
 dset
 
 
-#
+
 
 rm(dset,n.subjects,assignments)
 
@@ -219,7 +214,7 @@ gen.data <- function(n.subjects, control.mean, treatment.effect, sd) {
 }
 
 
-#
+
 #Exercise: use this function to generate a data set of size 20, with a treatment effect of 0, and a 
 #mean of 1.  Assume the standard deviation is 0.3.
 
@@ -238,7 +233,7 @@ t.test(outcome ~ treatment, data=v1)
 #What do you find?  Try it a few times---generate new data, and repeat the test.  Keep doing this until
 #you find $P<0.05$.  How long did it take?  When $P<0.05$, we reject the null hypothesis at a
 #significance level of 0.05.
-#
+
 #Exercise: repeat this 10,000 times.  Let's build a function to do this:
 
 
@@ -270,26 +265,26 @@ mean(a1 < 0.05)
 #Now, suppose the treatment does work.  Suppose that the difference between the treatment group and
 #the control group is 0.15, which turns out to be one and one half lines of vision on a traditional
 #chart.  
-#
+
 
 a2 <- repeat.test(10000, 20, 1, -0.15, 0.3)
 mean(a2 < 0.05)
 
-#
+
 #How often do you reject for a sample size of 20?  Try it for 50, 100, 200, and 500.  Can you find the
 #sample size so that you reject 80\% of the time (approximately)?  The probability of rejecting the
 #null hypothesis when you should reject it is called the power, and it does depend on the sample size.
-#
+
 #Now, suppose the treatment effect is very small, only 0.025:
 
 a3 <- repeat.test(10000, 100, 1, -0.025, 0.3)
 mean(a3 < 0.05)
 
-#
+
 #For a sample size of 100, how often do you reject?  For a given sample size, how does the chance of
 #rejecting the null hypothesis depend on the effect?  As you lower the treatment effect, the power goes
 #up or down?
-#
+
 #Inference
 #An important advantage of the clinical trial is that it (probabilistically) controls for confounding.  
 #Randomization allocates potential confounders to the two arms with equal probability.  Let's do an
@@ -317,7 +312,7 @@ simdata <- gen.data.ii(100, 1.0, 0.0, 0.3, 0.1, 0.4)
 table(simdata$treatment, simdata$severe)
 
 #What does this mean?  Repeat this several times.  
-#
+
 #Let's repeat the simulated trial several times under these circumstances.
 
 repeat.test.ii <- function(nsims, n.subjects, control.mean, treatment.effect, sd, prob.severe, severe.effect) {
