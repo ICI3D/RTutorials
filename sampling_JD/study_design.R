10c91f9f419a7347cfe43e7b94b2d4619fee8dc5
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-06-25 20:33:33 +0200

    modified:   study_design.R

--- sampling_JD/study_design.R
+++ sampling_JD/study_design.R
@@ -3,6 +3,9 @@
 ## R, before spending a lot of time and money on conducting the study in W
 
 ## What is a good research question?
-## 
+## These methods absolutely work for dynamical questions
+## But not necessarily in 90 minutes 🙂
 
 ## What are our goals for a successful study?
+
+41*2439
