fd280d9e95df03499ed1052b6b237f53535a86e2
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-06-14 07:08:05 -0400

    macpan calibration example

--- mpLabFitting.R
+++ mpLabFitting.R
@@ -40,6 +40,15 @@ hiv4spec = mp_tmb_model_spec(
 )
 
 ## What's a good way to quickly show the diagram but without misleading people about the flows to nowhere?
+##   SW: does this work for you?
+system.file("utils", "box-drawing.R", package = "macpan2") |> source()
+layout = mp_layout_grid(hiv4spec)
+(layout
+  |> plot_flow_diagram(show_flow_rates = TRUE)
+  |> draw_outflows(layout, show_labels = TRUE, lab = "rate")
+  |> draw_inflows(layout, show_labels = TRUE, lab = "rate")
+)
+
 
 ######################################################################
 
@@ -58,22 +67,26 @@ params = list(
 
 	, lambda0 = 0.65	## baseline transmission rate
 	, alpha = 1 ## non-linearity parameter
+)
 
-	# initial conditions
-	, S = 1 ## susceptible
-	, I1 = 1/400, I2 = 1/400, I3 = 1/400, I4 = 1/400 ## infectious per stage
+## initial conditions
+inits = list(
+	  S = 400 ## susceptible
+	, I1 = 1, I2 = 1, I3 = 1, I4 = 1 ## infectious per stage
 	, D = 0		 ## cumulated death due to disease
 )
 
 ## How would you calculate R0 for this model?
 ## FIXME
 
-hiv4impl = (hiv4spec
-	|> mp_tmb_update(default = params)
+hiv4init = (hiv4spec
 	|> mp_rk4()
-	|> mp_simulator(time_steps = 50L
-		, outputs=c("S", "I", "N", "P")
-	)
+	|> mp_tmb_update(default = params, inits = inits)
+)
+
+hiv4impl = mp_simulator(hiv4init
+	, time_steps = 50L
+	, outputs=c("S", "I", "N", "P", "D")
 )
 
 ######################################################################
@@ -106,7 +119,7 @@ print(ggplot(basePrev)
 
 ## But the inferred population effects are quite dramatic
 baseState <- (baseSim 
-	|> filter(matrix!="P") 
+	|> filter(matrix!="P")
 	|> rename(proportion=value, state=matrix)
 )
 
@@ -114,4 +127,77 @@ baseState <- (baseSim
 print(ggplot(baseState)
 	+ aes(time, proportion, color=state)
 	+ geom_line()
+)
+
+
+## Now we will try calibration. Please read this article before 
+## proceeding: https://canmod.github.io/macpan2/articles/calibration
+
+## To illustrate calibration, we add Poisson noise to the simulated number of 
+## infectious individuals. Then we will attempt to fit the model to 
+## these simulated data, to see if we can recover the parameters 
+## from the simulations.
+noisyI = (baseSim
+	|> filter(matrix == "I")
+	|> mutate(value = rpois(n(), value))
+)
+set.seed(1)
+(noisyI
+  |> rename(`Observation Year` = time)
+  |> rename(`Infectious Individuals` = value)
+  |> ggplot()
+  + geom_line(aes(`Observation Year`, `Infectious Individuals`))
+  + theme_bw()
+)
+
+## Before proceeding, think about how you might fit the model to these data
+## using the information you just read about in the article on calibration.
+
+## The first step is to create a calibrator object, which is an object that 
+## can be used to calibrate the parameters of a model specification to data.
+calibrator = (hiv4init
+	## Change the value of lambda0 so that the calibrator has to infer 
+	## the true value (0.65) from the noisy simulated data.
+  |> mp_tmb_update(default = list(lambda0 = 1))
+	|> mp_tmb_calibrator(
+        data = noisyI
+        
+        ## Assume that the noisy I is Poisson distributed
+      , traj = list(I = mp_poisson())
+      
+        ## The parameter to fit is the log transform of lambda0
+        ## (prefixing parameters by common transformation names
+        ## allows us to fit on the transformed scale)
+      , par = c("log_lambda0")
+  )
+)
+
+## Optimize parameters in the calibrator.
+## The mp_optimize() function modifies the sir_calibrator object in place,
+## updating the parameter values to their optimized estimates.
+mp_optimize(calibrator)
+
+## Recall that the true value of lambda0 is 0.65.
+print(mp_default_list(hiv4init)$lambda0)
+
+## Did calibration result in a reasonable estimate of this true lambda0.
+fittedCoefs = (mp_tmb_coef(calibrator, conf.int = TRUE)
+ |> select(-term, -row, -col, -type)
+)
+print(fittedCoefs)
+
+
+## The simulated data (black) that we fit to matches the predictions of the 
+## fitted model (red) with 95% confidence intervals for the point prediction).
+(calibrator
+	
+ ## macpan2 trajectory function that produces confidence intervals
+ ## using standard maximum likelihood theory.
+ |> mp_trajectory_sd(conf.int = TRUE)
+	
+ |> ggplot()
+ + geom_line(aes(time, value), colour = "red")
+ + geom_ribbon(aes(time, ymin = conf.low, ymax = conf.high), alpha = 0.2, fill = "red")
+ + geom_line(aes(time, value), data = noisyI)
+ + theme_bw()
 )
