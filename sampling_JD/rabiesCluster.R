bf6da9879e449dc0e01a37d6e7bed6340ee0036e
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-06-25 18:52:08 +0200

    new file:   rabiesCluster.R

---
+++ sampling_JD/rabiesCluster.R
@@ -0,0 +1,7 @@
+### Our rabies modeling shows that we expect rabies vaccination to reduce rabies rates even among unvaccinated dogs
+
+### We will think about how to design a trial that could test this assumption
+
+### We will do some simulated trials in our model world to see:
+#### Whether the test is valid (i.e., can we properly control the false-positive rate)
+#### Whether the test is powerful (i.e., if our assumptions are right, do we expect it to work
