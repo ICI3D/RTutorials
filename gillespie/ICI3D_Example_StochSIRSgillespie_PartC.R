b92ebb8884f46ce453020cbebb93c73fd86e9b3c
laurenbrown-98 <laurenbrown.lbrown@gmail.com>
2026-06-15 15:12:04 +0200

    Gillespie algorithm format & tidyverse updates

--- gillespie/ICI3D_Example_StochSIRSgillespie_PartC.R
+++ gillespie/ICI3D_Example_StochSIRSgillespie_PartC.R
@@ -1,53 +1,74 @@
-## Gillespie algorithm benchmark question: key for part (c)
-## Meaningful Modeling of Epidemiological Data, 2012
-## AIMS, Muizenberg
-## Juliet Pulliam, 2011,2012
+#####################################################################
 
-## The question leads you through the construction of a stochastic
-## SIRS model using the Gillespie algorithm.
-##
-## This is example code for part (c).
+## Example: Stochastic SIRS model using the Gillespie algorithm
+## Part C: SIRS model implementation
+
+#####################################################################
+
+## Clinic on the Meaningful Modeling of Epidemiological Data
+## International Clinics on Infectious Disease Dynamics and Data (ICI3D) Program
+## https://www.ici3d.org
+
+## Attribution: Juliet R.C. Pulliam (2009, 2011, 2012)
+## Last update: Lauren Brown (2026)
+
+## Some Rights Reserved
+## CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/)
+
+#####################################################################
 
 rm(list=ls())
 
-event <- function(time,S,I,R,N=pop.size,R0=basic.reproductive.ratio,rho=rate.of.waning){
+library(tidyverse)
 
-  trans.rate <- R0*S*I/N
-  recov.rate <- I
-  loss.rate <- rho*R
+## event(): advances the simulation by one Gillespie event and returns
+## the updated state as a tibble
+event <- function(time, S, I, R, params) {
+  with(as.list(params), {
+    trans.rate <- R0 * S * I / N
+    recov.rate <- I
+    loss.rate  <- rho * R
+    tot.rate   <- trans.rate + recov.rate + loss.rate
 
-  tot.rate <- trans.rate+recov.rate+loss.rate
-  
-  event.time <- time+rexp(1,tot.rate)
-  
-  dd <- runif(1)
-  if(dd<trans.rate/tot.rate){
-   	S <- S-1
-   	I <- I+1
-  }else{
-  	if(dd<(trans.rate+recov.rate)/tot.rate){
-  	  I <- I-1
-  	  R <- R+1  	  
-  	}else{
-  	  R <- R-1
-  	  S <- S+1
-  	}
-  }
-  
-  return(data.frame(time=event.time,S=S,I=I,R=R))
+    event.time <- time + rexp(1, tot.rate)
+
+    dd <- runif(1)
+    if (dd < trans.rate / tot.rate) {
+      S <- S - 1
+      I <- I + 1
+    } else if (dd < (trans.rate + recov.rate) / tot.rate) {
+      I <- I - 1
+      R <- R + 1
+    } else {
+      R <- R - 1
+      S <- S + 1
+    }
+
+    tibble(time=event.time, S=S, I=I, R=R)
+  })
 }
 
+params <- list(
+  N   = 1000,
+  R0  = 4,
+  rho = 0.01
+)
+MAXTIME <- 150
 
-pop.size <- 1000
-basic.reproductive.ratio <- 4
-rate.of.waning <- 0.01
+## Initialise and run the simulation, accumulating rows in a list
+## to avoid the cost of growing a data frame in the loop
 
-sim.time <- 0
-ts <- data.frame(time=sim.time,S=pop.size-1,I=1,R=0)
-next.time <- ts
-while(next.time$time<15&next.time$I>0){
-  next.time <- event(next.time$time,next.time$S,next.time$I,next.time$R)
-  ts <- rbind(ts,next.time)
+ts <- list(tibble(time=0, S=params$N - 1, I=1, R=0))
+current <- ts[[1]]
+i <- 1
+while (current$time < MAXTIME && current$I > 0) {
+  current <- event(current$time, current$S, current$I, current$R, params)
+  i <- i + 1
+  ts[[i]] <- current
 }
+ts <- bind_rows(ts)
 
-plot(ts$time,ts$I,type="l",xlab="Time",ylab="Number infectious",bty="n")
+ggplot(ts, aes(x=time, y=I)) +
+  geom_step() +
+  labs(x="Time", y="Number infectious") +
+  theme_classic(base_size=14)
