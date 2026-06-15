a0c89958e071d87902132b5b617ec6ac7f7c8faf
laurenbrown-98 <laurenbrown.lbrown@gmail.com>
2026-06-15 15:12:04 +0200

    Gillespie algorithm format & tidyverse updates

--- gillespie/ICI3D_Example_StochSIRSgillespie.R
+++ gillespie/ICI3D_Example_StochSIRSgillespie.R
@@ -1,12 +1,25 @@
-## Gillespie algorithm benchmark question
-## Meaningful Modeling of Epidemiological Data, 2012
-## AIMS, Muizenberg
-## Juliet Pulliam, 2009,2012
+#####################################################################
+
+## Example: Stochastic SIRS model using the Gillespie algorithm
+
+#####################################################################
+
+## Clinic on the Meaningful Modeling of Epidemiological Data
+## International Clinics on Infectious Disease Dynamics and Data (ICI3D) Program
+## https://www.ici3d.org
+
+## Attribution: Juliet R.C. Pulliam (2009, 2012)
+## Last update: Lauren Brown (2026)
+
+## Some Rights Reserved
+## CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/)
+
+#####################################################################
 
 ## This question leads you through the construction of a stochastic
 ## SIRS model using the Gillespie algorithm.
-##
-## A key is available for part (c).
+
+## Example code is available for parts (c) and (d).
 
 ## ODE models of infectious disease dynamics treat individuals as
 ## if they were continuous entities, which of course is not the
@@ -16,24 +29,27 @@
 ## individuals). The following model represents an infection that
 ## produces transient immunity in its host:
 
-## dS/dt = ro*R - R_0*S*I/N
+## dS/dt = rho*R - R0*S*I/N
+## dI/dt = R0*S*I/N - I
+## dR/dt = I - rho*R
 
-## dI/dt = R_0*S*I/N - I
+######################################################################
 
-## dR/dt = I - ro*R
+## a. What is the biological meaning of each of the parameters?
 
-#################### 
-## 	a. What is the biological meaning of each of the parameters?
+######################################################################
 
-#################### 
-## 	b. Fill in the following table:
+######################################################################
+
+## b. Fill in the following table:
+
+######################################################################
 
 ## EVENT                        RATE            TIME TO EVENT
 ## (S,I,R)->(S-1,I+1,R)         lambda_1 = ?    tau_1 = ?
 ## (S,I,R)->(S,I-1,R+1)         lambda_2 = ?    tau_2 = ?
 ## (S,I,R)->(S+1,I,R-1)         lambda_3 = ?    tau_3 = ?
 
-	
 ## The algorithm is implemented as follows:
 
 ## Step 1: Given your parameters and the current state of all
@@ -46,16 +62,19 @@
 ## with rate parameter lambda_event to determine the time to the next
 ## event, tau_event.
 
-## Step 3: Determine which event occurs at time t+tau_event , given
-## the probability that the event was event i is equal to
+## Step 3: Determine which event occurs at time t+tau_event, given
+## that the probability the event was event i is equal to
 ## lambda_i/lambda_event.
 
 ## Step 4: Update the values of all variables (t, S(t), I(t), R(t))
 ## and repeat steps 1-4.
 
-#################### 
+######################################################################
+
 ## c. Write an R script to implement the Gillespie algorithm for the
-## SIRS model described above, with the initial conditions
+## SIRS model described above, with the initial conditions:
+
+######################################################################
 
 ## S(t=0) = N-1
 ## I(t=0) = 1
@@ -63,15 +82,18 @@
 ## Include a time limit in your algorithm, telling it to stop when
 ## time reaches a maximum of t = 150 disease generations. Also include
 ## a plotting function in your script, and run the algorithm multiple
-## times for N=10,000 R0=4 and Ï=0.01, until you have an idea of the
+## times for N=1000, R0=4 and rho=0.01, until you have an idea of the
 ## different patterns it produces. Save plots of the number of
 ## infections through time for 3 realizations of your algorithm.
 
-#################### 
+######################################################################
+
 ## d. Modify the SIRS model above to include reintroduction of the
 ## infection from some external source, once every 5 disease
 ## generations (on average). Fill in the table for this new version of
 ## the model:
+
+######################################################################
 
 ## EVENT                        RATE            TIME TO EVENT
 ## (S,I,R)->(S-1,I+1,R)         lambda_1 = ?    tau_1 = ?
@@ -79,7 +101,6 @@
 ## (S,I,R)->(S+1,I,R-1)         lambda_3 = ?    tau_3 = ?
 
 ## Copy your R script to a new file and modify your algorithm for the
-## new model.  How does an external source of infection affect the
+## new model. How does an external source of infection affect the
 ## dynamics observed? Create plots that illustrate the differences
 ## between this model and the previous one.
-
--- gillespie/ICI3D_Example_StochSIRSgillespie_PartD.R
+++ gillespie/ICI3D_Example_StochSIRSgillespie_PartD.R
@@ -1,80 +1,93 @@
-## Gillespie algorithm benchmark question: key for part (d)
-## Meaningful Modeling of Epidemiological Data, 2012
-## AIMS, Muizenberg
-## Juliet Pulliam, 2011,2012
-##
-## The question leads you through the construction of a stochastic
-## SIRS model using the Gillespie algorithm.
-##
-## This is example code for part (d).
+#####################################################################
+
+## Example: Stochastic SIRS model using the Gillespie algorithm
+## Part D: SIRS model with external source of infection
+
+#####################################################################
+
+## Clinic on the Meaningful Modeling of Epidemiological Data
+## International Clinics on Infectious Disease Dynamics and Data (ICI3D) Program
+## https://www.ici3d.org
+
+## Attribution: Juliet R.C. Pulliam (2009, 2011, 2012)
+## Last update: Lauren Brown (2026)
+
+## Some Rights Reserved
+## CC BY-NC 4.0 (https://creativecommons.org/licenses/by-nc/4.0/)
+
+#####################################################################
 
 rm(list=ls())
 
-event <- function(time,S,I,R,N=pop.size,R0=basic.reproductive.ratio,rho=rate.of.waning,external=external.introduction.rate,final.time=MAXTIME){
-  
-  trans.rate <- R0*S*I/N + external
-  recov.rate <- I
-  loss.rate <- rho*R
-  
-  tot.rate <- trans.rate+recov.rate+loss.rate
-  
-  if(tot.rate==0){
-    return(data.frame(time=final.time,S=S,I=I,R=R))
-  }
-  
-  event.time <- time+rexp(1,tot.rate)
-  
-  dd <- runif(1)
-  if(dd<trans.rate/tot.rate){
-    S <- S-1
-    I <- I+1
-  }else{
-    if(dd<(trans.rate+recov.rate)/tot.rate){
-      I <- I-1
-      R <- R+1  	  
-    }else{
-      R <- R-1
-      S <- S+1
+library(tidyverse)
+
+## event(): advances the simulation by one Gillespie event, with an optional
+## external introduction rate; returns the updated state as a tibble
+
+event <- function(time, S, I, R, params, external=0, final.time=Inf) {
+  with(as.list(params), {
+    trans.rate <- R0 * S * I / N + external
+    recov.rate <- I
+    loss.rate  <- rho * R
+    tot.rate   <- trans.rate + recov.rate + loss.rate
+
+    if (tot.rate == 0) {
+      event.time <- final.time
+    } else {
+      event.time <- time + rexp(1, tot.rate)
+      dd <- runif(1)
+      if (dd < trans.rate / tot.rate) {
+        S <- S - 1
+        I <- I + 1
+      } else if (dd < (trans.rate + recov.rate) / tot.rate) {
+        I <- I - 1
+        R <- R + 1
+      } else {
+        R <- R - 1
+        S <- S + 1
+      }
     }
-  }
-  
-  return(data.frame(time=event.time,S=S,I=I,R=R))
+
+    tibble(time=event.time, S=S, I=I, R=R)
+  })
 }
 
-MAXTIME <- 100
-NUMRUNS <- 5
+## run_sirs(): runs one full simulation and returns a tibble labelled with
+## a run identifier and the model variant name
 
-pop.size <- 1000
-basic.reproductive.ratio <- 4
-rate.of.waning <- 0.01 # Note: Try increasing this value to 0.05... what happens?
-
-par(mfcol=c(2,1),mar=c(4,2,1,1))
-plot(NA,NA,xlim=c(0,MAXTIME),ylim=c(0,pop.size/2),xlab="Time",ylab="Number infectious",bty="n")
-
-# Version with external source of infection (external set to 1/5)
-for(ii in 1:NUMRUNS){
-  sim.time <- 0
-  ts <- data.frame(time=sim.time,S=pop.size-1,I=1,R=0)
-  next.time <- ts
-  while(next.time$time<MAXTIME){
-    next.time <- event(next.time$time,next.time$S,next.time$I,next.time$R,external=1/5)
-    ts <- rbind(ts,next.time)
+run_sirs <- function(run_id, external, params, MAXTIME) {
+  ts <- list(tibble(time=0, S=params$N - 1, I=1, R=0))
+  current <- ts[[1]]
+  i <- 1
+  while (current$time < MAXTIME) {
+    current <- event(current$time, current$S, current$I, current$R,
+                     params, external=external, final.time=MAXTIME)
+    i <- i + 1
+    ts[[i]] <- current
   }
-  
-  lines(ts$time,ts$I,col=ii)
+  bind_rows(ts) |> mutate(run=run_id)
 }
 
-plot(NA,NA,xlim=c(0,MAXTIME),ylim=c(0,pop.size/2),xlab="Time",ylab="Number infectious",bty="n")
+params <- list(
+  N   = 1000,
+  R0  = 4,
+  rho = 0.01  # Try increasing this to 0.05 -- what happens?
+)
+MAXTIME  <- 100
+NUMRUNS  <- 5
 
-# Version without external source of infection (external set to 0)
-for(ii in 1:NUMRUNS){
-  sim.time <- 0
-  ts <- data.frame(time=sim.time,S=pop.size-1,I=1,R=0)
-  next.time <- ts
-  while(next.time$time<MAXTIME){
-    next.time <- event(next.time$time,next.time$S,next.time$I,next.time$R,external=0)
-    ts <- rbind(ts,next.time)
-  }
-  
-  lines(ts$time,ts$I,col=ii)
-}
+## Collect runs for both model variants into a single tidy tibble
+runs <- bind_rows(
+  map_dfr(1:NUMRUNS, run_sirs, external=1/5, params=params, MAXTIME=MAXTIME) |>
+    mutate(model="With external source"),
+  map_dfr(1:NUMRUNS, run_sirs, external=0,   params=params, MAXTIME=MAXTIME) |>
+    mutate(model="Without external source")
+)
+
+ggplot(runs, aes(x=time, y=I, group=run, colour=factor(run))) +
+  geom_step() +
+  facet_wrap(~model, ncol=1) +
+  coord_cartesian(ylim=c(0, params$N / 2)) +
+  labs(x="Time", y="Number infectious") +
+  theme_classic(base_size=14) +
+  theme(legend.position="none")
